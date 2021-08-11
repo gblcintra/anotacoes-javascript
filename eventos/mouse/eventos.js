b32301385ddd8d38d200b606cc4ec0d0d3ccf72d
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2021-08-11 15:23:18 -0300

    feat(mouse): about event mouse

---
+++ eventos/mouse/eventos.js
@@ -0,0 +1,36 @@
+//Click do mouse
+function apertar(){
+    console.log('Apertou o mouse');
+}
+
+//Soltou do mouse
+function soltar(){
+    console.log('Soltou o mouse');
+}
+
+//Passar o mouse encima do botão
+function passar(){
+    console.log('Passou o mouse encima do botão');
+}
+
+//Tirou o mouse encima do botão
+function tirar(){
+    console.log('Tirou o mouse encima do botão');
+}
+
+//Mover o mouse encima do botão
+function mover(){
+    console.log('Moveu o mouse encima do botão');
+}
+
+//Clicar no botão
+function clicar(){
+    console.log('Clicou no mouse');
+}
+
+//Click com o botão direito do mouse
+function botaoDireito(){
+    console.log('Clicou no mouse com o botão direito');
+    //tirando a op;'ao no bot'ao direito do mouse
+    return false;
+}
