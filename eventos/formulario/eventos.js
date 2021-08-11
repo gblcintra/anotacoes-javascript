8e3317367d98966c720fcd03d487202343d03a21
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2021-08-11 15:26:17 -0300

    feat(form): about event forms

---
+++ eventos/formulario/eventos.js
@@ -0,0 +1,16 @@
+
+//Campo focado
+function focado(){
+    console.log('Campo focado');
+}
+
+//quando desfoca, sai do campo
+function desfocado(){
+    console.log('Campo desfocado');
+}
+
+//quando desfoca, sai do campo
+function mudanca(objeto){
+    //pegar o valor do objeto utiliza .value
+    console.log('Campo selecionado: ' + objeto.value);
+}
