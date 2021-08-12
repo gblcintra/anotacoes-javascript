e780a353b5f8e024f9f79cad7dd6922ad034d0c0
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2021-08-12 11:08:25 -0300

    feat(OO): about O.O.

--- orientacao-a-objeto/oo.js
+++ orientacao-a-objeto/oo.js
@@ -1,12 +1,30 @@
 //Criar um objeto
 
-function Carro(){
-    
+function Carro() {
+
     //this faz a referencia a esse objeto
     this.marca = "VolksWagen";
     this.nome = "Gol";
     this.ano = "2019";
     this.potencia = "110cv";
+    this.velocidadeAtual = 0;
+
+    this.ligar = () => {
+        console.log(this.nome + ' agora esta ligado');
+    }
+    this.andar = (velocidade) => {
+        this.velocidadeAtual = this.velocidadeAtual + velocidade;
+        console.log(this.nome + '  andando a ' + velocidade + 'Km/h');
+    }
+    this.parar = () => {
+        if (this.velocidadeAtual > 0) {
+            console.log(this.nome + ' parado');
+            this.velocidadeAtual = 0;
+        } else {
+            console.log(this.nome + ' ja esta parado');
+
+        }
+    }
 
 
 
@@ -17,4 +35,5 @@ carro1.potencia = "180cv";
 
 var carro2 = new Carro();
 carro2.ano = "2015";
-carro2.potencia = "90cv"
+carro2.nome = "Saveiro";
+carro2.potencia = "90cv";
