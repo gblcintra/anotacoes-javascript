e33ef1f20776bb121a7015c988f2752b767c8b95
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2021-08-13 16:32:15 -0300

    feat(class): about class

--- ES6+/classes/script.js
+++ ES6+/classes/script.js
@@ -29,3 +29,31 @@ document.getElementById('novo').onclick = function(){
 	// minhasTarefas.addTarefa();
 	minhasTarefas.add("Nova Tarefa");
 }
+
+
+///-------------------------------------
+class Pessoa{
+	constructor(){
+		this.nome = '';
+		this.sobrenome = '';
+	}
+	nomePessoa(primeiroNome){
+		this.nome = primeiroNome;
+		console.log('Meu nome é: ' + this.nome);
+	}
+	segundoNome(segundoNome){
+		this.sobrenome = segundoNome;
+		console.log('Meu sobrenome é: ' + this.sobrenome);
+	}
+	nomeCompleto(){
+		let nomeCompleto = this.nome + ' ' + this.sobrenome;
+		console.log(nomeCompleto)
+	}
+};
+
+let pessoa1 = new Pessoa();
+
+pessoa1.nomePessoa('Gabriel');
+pessoa1.segundoNome('Cintra');
+
+pessoa1.nomeCompleto();
